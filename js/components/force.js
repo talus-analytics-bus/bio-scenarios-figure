0f2b9ef678edfd96a9b0391dc3000b3fd4272727
jpecht <me.jefferson@gmail.com>
2017-09-18 14:23:15 -0600

    added chord diagram

--- js/components/force.js
+++ js/components/force.js
@@ -1,17 +1,65 @@
 (() => {
 	App.buildForceDiagram = (selector, nodeData, edgeData, param = {}) => {
-		const width = param.width || 1000;
-		const height = param.height || 500;
+		const margin = { top: 20, right: 20, bottom: 20, left: 20 };
+		const outerRadius = 200;
+		const innerRadius = outerRadius - 15;
+		const width = 2 * outerRadius;
+		const height = 2 * outerRadius;
 
-		const network = d3.select(selector).append('svg')
-			.attr('width', width)
-			.attr('height', height);
+		const chart = d3.select(selector).append('svg')
+			.attr('width', width + margin.left + margin.right)
+			.attr('height', height + margin.top + margin.bottom)
+			.append('g')
+				.attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+
+		/* --------- Chord Part --------- */
+		// establish chord stuff
+		const chord = d3.chord()
+			.padAngle(0.05);
+		const arc = d3.arc()
+			.innerRadius(innerRadius)
+			.outerRadius(outerRadius);
+		const ribbon = d3.ribbon()
+			.radius(innerRadius);
+
+		const matrix = [
+			[11975,  5871, 8916, 2868],
+			[ 1951, 10048, 2060, 6171],
+			[ 8010, 16145, 8090, 8045],
+			[ 1013,   990,  940, 6907],
+		];
+
+		const chordG = chart.append('g')
+			.attr('transform', `translate(${outerRadius}, ${outerRadius})`)
+			.datum(chord(matrix));
+		const chordGroups = chordG.append('g')
+			.attr('class', 'groups')
+			.selectAll('g')
+				.data(d => d.groups)
+				.enter().append('g');
+		chordGroups.append('path')
+			.style('fill', 'steelblue')
+			.attr('d', arc);
+
+		chordG.append('g')
+			.attr('class', 'ribbons')
+			.selectAll('path')
+				.data(d => d)
+				.enter().append('path')
+					.attr('d', ribbon)
+					.style('fill', 'lightsteelblue')
+					.style('stroke', 'rgba(0,0,0,0.5)');
+
+
+		/* --------- Force Part --------- */
 		const simulation = d3.forceSimulation()
 			.velocityDecay(param.velocityDecay || 0.2)
 			.force('link', d3.forceLink().id(d => d.id))
 			.force('charge', d3.forceManyBody().strength(-2))
 			.force('center', d3.forceCenter(width / 2, height / 2));
 
+		// establish scales
 		const radiusScale = d3.scaleLinear()
 			.domain([0, 10])
 			.range([5, 20]);
@@ -23,13 +71,13 @@
 
 
 		// draw nodes and links
-		const link = network.append('g')
+		const link = chart.append('g')
 			.attr('class', 'links')
 			.selectAll('.link')
 				.data(edgeData)
 				.enter().append('line')
 					.attr('class', 'link');
-		const node = network.append('g')
+		const node = chart.append('g')
 			.attr('class', 'nodes')
 			.selectAll('.node')
 				.data(nodeData)
