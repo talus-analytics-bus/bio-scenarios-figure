149a4ec0ce6ba403be2da11d65fb357520703625
jpecht <me.jefferson@gmail.com>
2017-09-21 11:09:21 -0600

    switched to pack; looks good now

--- js/components/force.js
+++ js/components/force.js
@@ -1,7 +1,5 @@
 (() => {
-	//App.buildForceDiagram = (selector, nodeData, edgeData, nodeDataJson, param = {}) => {
-    App.buildForceDiagram = (selector, nodeData, edgeData, param = {}) => {
-
+	App.buildForceDiagram = (selector, data, param = {}) => {
 
 		const margin = { top: 20, right: 20, bottom: 20, left: 20 };
 		const outerRadius = 250;
@@ -9,21 +7,52 @@
 		const width = 2 * outerRadius;
 		const height = 2 * outerRadius;
 
-		const chart = d3.select(selector).append('svg')
+		const chartContainer = d3.select(selector).append('svg')
 			.attr('width', width + margin.left + margin.right)
 			.attr('height', height + margin.top + margin.bottom)
-			.append('g')
-				.attr('transform', `translate(${outerRadius + margin.left}, ${outerRadius + margin.top})`);
+		const chart = chartContainer.append('g')
+			.attr('transform', `translate(${outerRadius + margin.left}, ${outerRadius + margin.top})`);
+
+		// add glow definition
+		const defs = chartContainer.append('defs');
+		const filter = defs.append('filter')
+			.attr('id', 'glow');
+		filter.append("feGaussianBlur")
+			.attr("stdDeviation","3.5")
+			.attr("result","coloredBlur");
+		var feMerge = filter.append("feMerge");
+		feMerge.append("feMergeNode")
+			.attr("in","coloredBlur");
+		feMerge.append("feMergeNode")
+			.attr("in","SourceGraphic");
+
+		// add different groups
 		const ribbonG = chart.append('g');
 		const chordG = chart.append('g');
 		const nodeG = chart.append('g');
 
 
 		/* --------- Chord Part --------- */
+		// chord data
+		const chordData = [
+			'Spread Modality',
+			'Route of Transmission',
+			'Affected Population',
+			'Diagnostics',
+			'Medical Countermeasures',
+			'Personal Protective Equipment',
+			'Morbidity',
+			'Response Level',
+			'Policy Measures',
+			'Stakeholders',
+			'Event Location',
+			'Natural, Intentional, Accidental Origin',
+		];
+
 		// establish chord stuff
 		const chord = d3.chord()
 			.padAngle(0.05)
-            .sortSubgroups(d3.descending);
+			.sortSubgroups(d3.descending);
 
 		const arc = d3.arc()
 			.innerRadius(innerRadius)
@@ -32,50 +61,31 @@
 		const chordColorScale = d3.scaleOrdinal()
 			.domain(d3.range(12))
 			.range(['#EADAF0',
-                '#D6B7E1',
-                '#C293D1',
-                '#A35EBA',
-                '#DFC8E8',
-                '#F4EDF7',
-                '#B7C2E1',
-                '#EDEFF7',
-                '#bde4fc',
-                '#3963bd',
-                '#4C64B2',
-                '#4B2559',
-                '#3D508F',
-                '#35467D',
-                '#272c58',
-                '#2D1636',]);
+				'#D6B7E1',
+				'#C293D1',
+				'#A35EBA',
+				'#DFC8E8',
+				'#F4EDF7',
+				'#B7C2E1',
+				'#EDEFF7',
+				'#bde4fc',
+				'#3963bd',
+				'#4C64B2',
+				'#4B2559',
+				'#3D508F',
+				'#35467D',
+				'#272c58',
+				'#2D1636',]);
 
-		const matrix = [
-			[200, 6295, 893, 91, 1820, 4245, 1176, 1399, 1157, 7586, 1616, 1870],
-			[1685, 4266, 3858, 2929, 7621, 9230, 1051, 1116, 701, 1749, 8886, 853],
-			[8048, 4055, 5836, 5001, 10799, 12662, 11745, 5029, 16973, 18479, 2722, 13652],
-			[12958, 7227, 14303, 1310, 13314, 5413, 7843, 17878, 536, 9371, 14537, 17700],
-			[10362, 15302, 5379, 2834, 15747, 19423, 11276, 18460, 17491, 9648, 2419, 8974],
-			[5004, 3956, 15488, 16646, 2614, 10712, 9502, 10110, 306, 12531, 12050, 12052],
-			[19144, 14701, 6777, 10999, 14988, 5205, 8200, 11717, 4032, 18179, 15831, 10649],
-			[12496, 846, 11719, 9697, 3007, 17794, 8242, 19855, 16990, 6714, 2999, 4658],
-			[9774, 9250, 9730, 8145, 14544, 15329, 13342, 6569, 8838, 5778, 17125, 11046],
-			[18486, 4791, 17489, 15943, 17331, 2337, 19419, 7972, 15708, 7965, 16729, 17216],
-			[1251, 19170, 2970, 11084, 9388, 10915, 124, 10930, 13946, 13175, 17185, 1338],
-			[18505, 4456, 1411, 11245, 4103, 2165, 19720, 10395, 16880, 3863, 3897, 519]
-		];
-
-		/*const arraySize = 12;
-		const matrix = [];
-		const maxSize = 20000;
-		let row = [];
-		for (let i = 0; i < arraySize; i++) {
-			row = [];
-			for (let j = 0; j < arraySize; j++) {
-				row.push(Math.floor(Math.random() * maxSize));
+		const chordValues = chordData.map((c) => {
+			const groupValues = [];
+			for (let i = 0; i < chordData.length; i++) {
+				groupValues.push(Math.round(1000 * Math.random()));
 			}
-			matrix.push(row);
-		}*/
+			return groupValues;
+		})
 
-		chordG.datum(chord(matrix));
+		chordG.datum(chord(chordValues));
 		const chordGroups = chordG.append('g')
 			.attr('class', 'groups')
 			.selectAll('g')
@@ -87,143 +97,91 @@
 
 
 		/* --------- Force Part --------- */
-		function forceNodes(nodeData, edgeData, forceCenterX, forceCenterY, nodeColor) {
-            const simulation = d3.forceSimulation()
-                .velocityDecay(param.velocityDecay || 0.2)
-                .force('link', d3.forceLink().id(d => d.id))
-                .force('charge', d3.forceManyBody().strength(-3))
-                .force("collide", d3.forceCollide(12))
-                .force('center', d3.forceCenter(forceCenterX, forceCenterY));
-                //.force('x', d3.forceX(forceCenterX));
-                //.force('y', d3.forceX(forceCenterY));
+		function createNodePack(rawNodeData, center, nodeColor) {
+			const size = 150;
+			const pack = d3.pack()
+				.size([size, size])
+				.padding(2);
 
-            // establish scales
-            const radiusScale = d3.scaleLinear()
-                .domain([0, 10])
-                .range([5, 22]);
-            const colorScale = d3.scaleLinear()
-                .domain([0, 50])
-                .range(['#082b84', '#c91414']);
+			const offsetX = center[0] - size / 2;
+			const offsetY = center[1] - size / 2;
 
+			const root = d3.hierarchy(rawNodeData)
+				.sum(d => d.size)
+				.sort((a, b) => b.value - a.value);
 
-            // draw nodes and links
-            const link = nodeG.append('g')
-                .attr('class', 'links')
-                .selectAll('.link')
-                .data(edgeData.nodes)
-                //.data(edgeData)
-                .enter().append('line')
-                .attr('class', 'link');
+			let focus = root;
+			const nodePackData = pack(root).descendants();
 
-            const node = nodeG.append('g')
-                .attr('class', 'nodes')
-                .selectAll('.node')
-                .data(nodeData.nodes)
-                .enter().append('circle')
-                .attr('class', 'node')
-                .attr('r', d => radiusScale(d.size))
-                //.style('fill', (d, i) => colorScale(i))
-                .style('fill', nodeColor)
+			const nodeContainer = nodeG.append('g')
+				.attr('transform', `translate(${offsetX}, ${offsetY})`);
 
-                .each(function addTooltip(d) {
-                    $(this).tooltipster({
-                        trigger: 'hover',
-                        content: d.id + ":<br>"+ d.label,
-                    });
-                })
-                .on('mouseover', (d) => {
-                    d3.selectAll('.ribbon')
-                        .filter(dd => dd.id === d.id)
-                        .classed('active', true);
-                })
-                .on('mouseout', (d) => {
-                    d3.selectAll('.ribbon').classed('active', false);
-                });
+			const nodes = nodeContainer.selectAll('g')
+				.data(nodePackData)
+				.enter().append('g')
+					.attr('transform', d => `translate(${d.x}, ${d.y})`)
+					.each(function(d) { d.node = this; });
 
-            // start simulation
-            const numTicks = 50;
-            let t = 0;
-            simulation
-                .nodes(nodeData.nodes)
-                .on('tick', () => {
-                    link
-                        .attr('x1', d => getConfinedX(d.source.x, d.source.y))
-                        .attr('y1', d => getConfinedY(d.source.x, d.source.y))
-                        .attr('x2', d => getConfinedX(d.target.x, d.target.y))
-                        .attr('y2', d => getConfinedY(d.target.x, d.target.y));
-                    node
-                        .attr('cx', d => getConfinedX(d.x, d.y))
-                        .attr('cy', d => getConfinedY(d.x, d.y));
+			nodes.append('circle')
+				.attr("class", (d) => {
+					return d.parent ? d.children ? "node" : "node node--leaf" : "node node--root";
+				})
+				.attr('r', d => d.r)
+				.filter(d => d.parent)
+					.style('fill', nodeColor)
+					.style('filter', 'url(#glow)')
+					.each(function(d) {
+						$(this).tooltipster({
+							trigger: 'hover',
+							contentAsHTML: true,
+							content: `<b>${d.data.id}:</b> ${d.data.label}`,
+						})
+					})
+					.on('mouseover', function onMouseover(d) {
+						d3.selectAll('.ribbon').style('opacity', (rd) => {
+							return rd.data.id === d.data.id ? 0.8 : 0.1;
+						});
+					})
+					.on('mouseout', function onMouseout() {
+						d3.selectAll('.ribbon').style('opacity', 0.5);
+					});
 
-                    d3.selectAll('.ribbon')
-                        .attr('d', ribbon);
+			// draw ribbons between nodes and chords
+			const ribbon = d3.ribbon()
+				.source(d => {
+					const x = offsetX + d.x;
+					const y = offsetY + d.y;
+					const dist = Math.sqrt(x * x + y * y);
+					const angle = (Math.PI / 2) - Math.atan2(-y, x);
+					return {
+						startAngle: angle,
+						endAngle: angle,
+						radius: dist,
+					};
+				})
+				.target(d => {
+					const angle = 2 * Math.PI * Math.random();
+					return {
+						startAngle: angle,
+						endAngle: angle + 0.1,
+						radius: innerRadius,
+					};
+				});
 
-                    // stop simulation if number of ticks has been exceeded
-                    t++;
-                    if (t === numTicks) simulation.stop();
-                });
-            simulation.force('link').links(edgeData.nodes);
-
-
-            // draw ribbons between nodes and chords
-            const ribbon = d3.ribbon()
-                .source(d => {
-                    const x = getConfinedX(d.x, d.y);
-                    const y = getConfinedY(d.x, d.y);
-                    const dist = Math.sqrt(x * x + y * y);
-                    const angle = (Math.PI / 2) - Math.atan2(-y, x);
-                    return {
-                        startAngle: angle,
-                        endAngle: angle,
-                        radius: dist,
-                    };
-                })
-                .target(d => {
-                    const angle = 2 * Math.PI * Math.random();
-                    return {
-                        startAngle: angle,
-                        endAngle: angle + 0.1,
-                        radius: innerRadius,
-                    };
-                });
-
-            ribbonG.append('g')
-                .attr('class', 'ribbons')
-                .selectAll('.ribbon')
-                .data(nodeData.nodes)
-                .enter().append('path')
-                .attr('class', 'ribbon')
-                .attr('d', ribbon)
-                .style('fill', 'lightsteelblue')
-                .style('stroke', 'rgba(0,0,0,0.5)');
-        }
-
-
-		function getConfinedX(x, y) {
-			const x1 = x - outerRadius;
-			const y1 = y - outerRadius;
-			const dist = Math.sqrt(x1 * x1 + y1 * y1);
-			const confinedRadius = innerRadius - 30;
-			if (dist < confinedRadius) return x;
-			if (x1 > 0) return x - 10;
-			return x + 10;
-		}
-
-		function getConfinedY(x, y) {
-			const x1 = x - outerRadius;
-			const y1 = y - outerRadius;
-			const dist = Math.sqrt(x1 * x1 + y1 * y1);
-			const confinedRadius = innerRadius - 30;
-			if (dist < confinedRadius) return y;
-			if (y1 > 0) return y - 10;
-			return y + 10;
+			ribbonG.append('g').selectAll('.ribbon')
+				.data(nodePackData.filter(d => d.parent))
+				.enter().append('path')
+					.attr('class', 'ribbon')
+					.attr('d', ribbon)
+					.style('fill', nodeColor)
+					.style('opacity', 0.5);
+					//.style('stroke', 'rgba(0,0,0,0.5)');
 		}
 
 
-        // start the forcing here
-        forceNodes(nodeData[0], edgeData[0], 120, 20, "#082B84");
-        forceNodes(nodeData[1], edgeData[1], 0, 60, "#C91414");
-        forceNodes(nodeData[2], edgeData[2], -80, -80, "#552257");
-
+		// create packs
+		createNodePack(data[0], [120, 0], "#082B84");
+		createNodePack(data[1], [-20, 100], "#C91414");
+		createNodePack(data[2], [-80, -80], "#552257");
 	};
 })();
--- js/pages/home.js
+++ js/pages/home.js
@@ -1,21 +1,7 @@
 (() => {
 	App.initHome = () => {
-		d3.queue()
-			/*.defer(d3.tsv, 'data/network_nodes.tsv')
-			.defer(d3.tsv, 'data/network_edges.tsv')*/
-            .defer(d3.json, 'data/network_nodes.json')
-        	.defer(d3.json, 'data/network_edges.json')
-			.await((error, nodeData, edgeData) => {
-                /*d3.json("data/network_nodes.json", function(error, json) {
-                    if (error) throw error;
-
-                    nodeDataJson = json;
-
-
-                    App.buildForceDiagram('.network-map', nodeData, edgeData, nodeDataJson);
-                });*/
-                App.buildForceDiagram('.network-map', nodeData, edgeData);
-
-			});
+	   d3.json('data/network_nodes.json', (error, data) => {
+			App.buildForceDiagram('.network-map', data);
+		});
 	};
 })();
