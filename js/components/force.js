608830d46248a2d99729385ee66b1f0c84737bb8
jpecht <me.jefferson@gmail.com>
2017-09-21 16:00:07 -0600

    complete revamp

--- js/components/force.js
+++ js/components/force.js
@@ -1,9 +1,8 @@
 (() => {
 	App.buildForceDiagram = (selector, data, param = {}) => {
-
-		const margin = { top: 20, right: 20, bottom: 20, left: 20 };
+		const margin = { top: 50, right: 150, bottom: 50, left: 150 };
 		const outerRadius = 250;
-		const innerRadius = outerRadius - 15;
+		const innerRadius = outerRadius - 20;
 		const width = 2 * outerRadius;
 		const height = 2 * outerRadius;
 
@@ -28,77 +27,84 @@
 
 		// add different groups
 		const ribbonG = chart.append('g');
-		const chordG = chart.append('g');
+		const arcG = chart.append('g');
 		const nodeG = chart.append('g');
 
 
-		/* --------- Chord Part --------- */
-		// chord data
-		const chordData = [
-			'Spread Modality',
-			'Route of Transmission',
-			'Affected Population',
-			'Diagnostics',
-			'Medical Countermeasures',
-			'Personal Protective Equipment',
-			'Morbidity',
-			'Response Level',
-			'Policy Measures',
-			'Stakeholders',
-			'Event Location',
-			'Natural, Intentional, Accidental Origin',
-		];
+		/* --------- Data Section --------- */
+		// calculate sum of values of all parameters
+		const numTotalValues = d3.sum(data, d => d.values.length);
 
-		// establish chord stuff
-		const chord = d3.chord()
-			.padAngle(0.05)
-			.sortSubgroups(d3.descending);
+		// assign start and end angle to arcs
+		const arcData = [];
+		const arcPadding = 0.05;
+		let runningTheta = 0;
+		data.forEach((d) => {
+			const totalTheta = 2 * Math.PI * (d.values.length / numTotalValues);
+			let dtheta = totalTheta - 2 * arcPadding;
+			if (dtheta < 0) dtheta = 0;
 
+			d.values.forEach((v, i) => {
+				const theta0 = runningTheta + arcPadding;
+				arcData.push({
+					parameter: d.name,
+					value: v,
+					index: i,
+					theta0: theta0 + (i * dtheta / d.values.length),
+					theta1: theta0 + ((i + 1) * dtheta / d.values.length),
+				});
+			});
+
+			d.theta0 = runningTheta;
+			runningTheta += totalTheta;
+			d.theta1 = runningTheta;
+			d.avgTheta = (d.theta1 + d.theta0) / 2;
+		});
+
+
+		/* --------- Arc Section --------- */
 		const arc = d3.arc()
 			.innerRadius(innerRadius)
-			.outerRadius(outerRadius);
+			.outerRadius(outerRadius)
+			.padAngle(0)
+			.startAngle(d => d.theta0)
+			.endAngle(d => d.theta1);
 
-		const chordColorScale = d3.scaleOrdinal()
-			.domain(d3.range(12))
-			.range(['#EADAF0',
-				'#D6B7E1',
-				'#C293D1',
-				'#A35EBA',
-				'#DFC8E8',
-				'#F4EDF7',
-				'#B7C2E1',
-				'#EDEFF7',
-				'#bde4fc',
-				'#3963bd',
-				'#4C64B2',
-				'#4B2559',
-				'#3D508F',
-				'#35467D',
-				'#272c58',
-				'#2D1636',]);
+		const arcColorScale = d3.scaleOrdinal()
+			.range(['#f7fbff', '#e1e8f1', '#cbd6e4', '#b5c3d6', '#a0b1c9', '#8a9ebb',
+				'#748cae', '#5e79a0', '#496793', '#335485', '#1d4278', '#08306b'].reverse());
 
-		const chordValues = chordData.map((c) => {
-			const groupValues = [];
-			for (let i = 0; i < chordData.length; i++) {
-				groupValues.push(Math.round(1000 * Math.random()));
-			}
-			return groupValues;
-		})
-
-		chordG.datum(chord(chordValues));
-		const chordGroups = chordG.append('g')
-			.attr('class', 'groups')
-			.selectAll('g')
-				.data(d => d.groups)
-				.enter().append('g');
-		chordGroups.append('path')
-			.style('fill', (d, i) => chordColorScale(i))
-			.attr('d', arc);
+		arcG.selectAll('.arc')
+			.data(arcData)
+			.enter().append('path')
+				.attr('class', 'arc')
+				.attr('d', arc)
+				.style('fill', d => arcColorScale(d.index))
+				.each(function addTooltip(d) {
+					$(this).tooltipster({
+						trigger: 'hover',
+						contentAsHTML: true,
+						content: `<b>${d.parameter}:</b> ${d.value}`,
+					});
+				});
+		arcG.selectAll('.arc-label')
+			.data(data)
+			.enter().append('text')
+				.attr('class', 'arc-label')
+				.attr('x', d => (outerRadius + 5) * Math.sin(d.avgTheta))
+				.attr('y', d => (outerRadius + 5) * -Math.cos(d.avgTheta))
+				.style('text-anchor', (d) => {
+					if (d.avgTheta > (Math.PI / 8) && d.avgTheta < (7 * Math.PI / 8)) return 'start';
+					if (d.avgTheta > (9 * Math.PI / 8) && d.avgTheta < (15 * Math.PI / 8)) return 'end';
+					return 'middle';
+				})
+				.text(d => d.name);
 
 
 		/* --------- Force Part --------- */
-		function createNodePack(rawNodeData, center, nodeColor) {
-			const size = 150;
+		function createNodePack(nodeData, center, nodeColor) {
+			// start drawing the node pack
+			const size = 300;
 			const pack = d3.pack()
 				.size([size, size])
 				.padding(2);
@@ -106,7 +112,7 @@
 			const offsetX = center[0] - size / 2;
 			const offsetY = center[1] - size / 2;
 
-			const root = d3.hierarchy(rawNodeData)
+			const root = d3.hierarchy(nodeData)
 				.sum(d => d.size)
 				.sort((a, b) => b.value - a.value);
 
@@ -131,26 +137,55 @@
 					.style('fill', nodeColor)
 					.style('filter', 'url(#glow)')
 					.each(function(d) {
+						const contentContainer = d3.select(document.createElement('div'));
+						const content = contentContainer.append('div');
+						const contentTitle = content.append('div')
+							.attr('class', 'tooltip-title')
+							.text(d.data.id);
+						let textClass = 'text-success';
+						if (d.data.size >= 8) textClass = 'text-danger';
+						else if (d.data.size >= 5) textClass = 'text-warning';
+						content.append('div')
+							.attr('class', 'tooltip-line')
+							.html(`<b>Extremity:</b> <b class="${textClass}">${d.data.size} out of 10</b>`);
+						d.data.links.forEach((l) => {
+							content.append('div')
+								.attr('class', 'tooltip-line')
+								.html(`<b>${l.parameter}:</b> ${l.value}`);
+						});
+
 						$(this).tooltipster({
 							trigger: 'hover',
 							contentAsHTML: true,
-							content: `<b>${d.data.id}:</b> ${d.data.label}`,
+							minWidth: 300,
+							content: contentContainer.html(),
 						})
-					})
-					.on('mouseover', function onMouseover(d) {
+					});
+					/*.on('mouseover', function onMouseover(d) {
 						d3.selectAll('.ribbon').style('opacity', (rd) => {
 							return rd.data.id === d.data.id ? 0.8 : 0.1;
 						});
 					})
 					.on('mouseout', function onMouseout() {
 						d3.selectAll('.ribbon').style('opacity', 0.5);
+					});*/
+
+			// make ribbon data
+			const ribbonData = [];
+			nodePackData.filter(d => d.parent).forEach((d) => {
+				d.data.links.forEach((label) => {
+					ribbonData.push({
+						source: Object.assign({}, d),
+						target: label,
 					});
+				});
+			});
 
 			// draw ribbons between nodes and chords
 			const ribbon = d3.ribbon()
-				.source(d => {
-					const x = offsetX + d.x;
-					const y = offsetY + d.y;
+				.source((d) => {
+					const x = offsetX + d.source.x;
+					const y = offsetY + d.source.y;
 					const dist = Math.sqrt(x * x + y * y);
 					const angle = (Math.PI / 2) - Math.atan2(-y, x);
 					return {
@@ -159,29 +194,64 @@
 						radius: dist,
 					};
 				})
-				.target(d => {
-					const angle = 2 * Math.PI * Math.random();
+				.target((d) => {
+					const arc = chart.selectAll('.arc')
+						.filter((a) => {
+							return a.parameter === d.target.parameter && a.value === d.target.value;
+						})
+						.data()[0];
 					return {
-						startAngle: angle,
-						endAngle: angle + 0.1,
+						startAngle: arc.theta0,
+						endAngle: arc.theta1,
 						radius: innerRadius,
 					};
 				});
 
 			ribbonG.append('g').selectAll('.ribbon')
-				.data(nodePackData.filter(d => d.parent))
+				.data(ribbonData)
 				.enter().append('path')
 					.attr('class', 'ribbon')
 					.attr('d', ribbon)
-					.style('fill', nodeColor)
-					.style('opacity', 0.5);
-					//.style('stroke', 'rgba(0,0,0,0.5)');
+					.style('fill', 'none')
+					.style('opacity', 0.01);
 		}
 
+		// create node data
+		const nodeData = {
+			name: 'background',
+			children: []
+		};
+		const indexArray = data.map(d => 0);
+		let nodeNum = 0;
+
+		function loopIndexAndAdd(indexToLoop, numTimes) {
+			for (let i = 0; i < numTimes; i++) {
+				indexArray[indexToLoop] = i;
+				if (indexToLoop === indexArray.length - 1) {
+					const node = {
+						id: `Scenario ${nodeNum}`,
+						size: Math.ceil(10 * Math.random()),
+						links: [],
+					};
+					const ia = indexArray.slice(0);
+					ia.forEach((valueIndex, j) => {
+						const parameter = data[j].name;
+						const value = data[j].values[valueIndex];
+						node.links.push({ parameter, value });
+					});
+					nodeData.children.push(node);
+					nodeNum++;
+				} else {
+					loopIndexAndAdd(indexToLoop + 1, data[indexToLoop + 1].values.length);
+				}
+			}
+		}
+		loopIndexAndAdd(0, data[0].values.length);
 
 		// create packs
-		createNodePack(data[0], [120, 0], "#082B84");
-		createNodePack(data[1], [-20, 100], "#C91414");
-		createNodePack(data[2], [-80, -80], "#552257");
+		/*createNodePack(nodeData, [120, 0], "#082B84");
+		createNodePack(nodeData, [-20, 100], "#C91414");
+		createNodePack(nodeData, [-80, -80], "#552257");*/
+		createNodePack(nodeData, [0, 0], '#c91414');
 	};
 })();
--- js/pages/home.js
+++ js/pages/home.js
@@ -1,7 +1,56 @@
 (() => {
 	App.initHome = () => {
-	   d3.json('data/network_nodes.json', (error, data) => {
-			App.buildForceDiagram('.network-map', data);
-		});
+		const data = [
+			{
+				name: 'Spread Modality',
+				values: ['Contagious', 'Non-contagious'],
+			},
+			{
+				name: 'Route of Transmission',
+				values: ['Airborne', 'Waterborne', 'Foodborne', 'Bloodborne'],
+			},
+			{
+				name: 'Affected Population',
+				values: ['All', 'Pregnant Women', 'Children', 'Elderly', 'Able-bodied'],
+			},
+			{
+				name: 'Medical Countermeasures',
+				values: ['Antivirals', 'Antibiotics', 'Vaccine', 'Post-exposure Prophylaxis'],
+			},
+			/*{
+				name: 'Personal Protective Equipment',
+				values: ['Mask, Gloves, Gown', 'Respirator', 'Containment Suit'],
+			},
+			{
+				name: 'Policy Measures',
+				values: ['National', 'International', 'None'],
+			},
+			{
+				name: 'Stakeholders',
+				values: ['Medical and Public Health', 'Law Enforcement', 'Logistics', 'Trade'],
+			},
+			{
+				name: 'Event Location',
+				values: ['Permissive', 'Non-permissive', 'Unstable', 'State controlled with access', 'State controlled without access'],
+			},
+			/*{
+				name: 'Natural, Intentional, Accidental Origin',
+				values: ['Natural', 'Accidental', 'Deliberate'],
+			},
+			{
+				name: 'Diagnostics',
+				values: ['Point of Care', 'BSL1', 'BSL2', 'BSL3', 'BSL4'],
+			},
+			{
+				name: 'Morbidity',
+				values: ['Low', 'Medium', 'Severe', 'Short-term', 'Long-term'],
+			},
+			{
+				name: 'Response Level',
+				values: ['Local', 'Intermediate', 'National', 'Regional', 'Global'],
+			},*/
+		];
+
+		App.buildForceDiagram('.network-map', data);
 	};
 })();
