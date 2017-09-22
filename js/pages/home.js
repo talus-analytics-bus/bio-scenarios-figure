6e3fc9d995f597119016f60595bd060f5eadf9c9
jpecht <me.jefferson@gmail.com>
2017-09-22 15:50:39 -0600

    filters work now

--- js/pages/home.js
+++ js/pages/home.js
@@ -34,38 +34,44 @@
 		$('select').multiselect({
 			//includeSelectAllOption: true,
 			numberDisplayed: 0,
-			onChange: (option, selected) => {
-				const val = option.val();
-				let parameter = '';
-				for (let k = 0; k < data.length; k++) {
-					if (data[k].values.includes(val)) {
-						parameter = data[k].name;
-						break;
-					}
-				}
-
-				d3.selectAll('.arc')
-					.filter(d => d.value === val)
-					.style('fill', selected ? 'steelblue' : '#bbb');
-				d3.selectAll('.node')
-					.filter((d) => {
-						if (!d.parent) return false;
-						const obj = d.data.links.find(l => l.parameter === parameter);
-						return (obj && obj.value === val);
-					})
-					.style('display', selected ? 'inline' : 'none')
-					.each((d) => {
-						d3.selectAll('.ribbon')
-							.filter(r => r.source.data.id === d.data.id)
-							.style('display', selected ? 'inline' : 'none');
-					});
-			},
+			onChange: updateDisplay,
 			onSelectAll: updateDisplay,
 			onDeselectAll: updateDisplay,
 		});
 
 		function updateDisplay() {
-			
+			d3.selectAll('.arc').style('fill', (d) => {
+				let isSelected = false;
+
+				d3.selectAll('select').each(function() {
+					const parameter = $(this).attr('name');
+					const values = $(this).val();
+					if (values.includes(d.value)) isSelected = true;
+				});
+
+				return isSelected ? 'steelblue' : '#bbb';
+			});
+
+			d3.selectAll('.node')
+				.filter(d => d.parent)
+				.style('display', (d) => {
+					let isSelected = true;
+
+					d3.selectAll('select').each(function() {
+						const parameter = $(this).attr('name');
+						const values = $(this).val();
+						const obj = d.data.links.find(l => l.parameter === parameter);
+						if (!values.includes(obj.value)) isSelected = false;
+					});
+
+					// toggle ribbons first
+					d3.selectAll('.ribbon')
+						.filter(r => r.source.data.id === d.data.id)
+						.style('display', isSelected ? 'inline' : 'none');
+
+
+					return isSelected ? 'inline' : 'none';
+				});
 		}
 	};
 })();
